71b8fa5a949d58e76fd83339e0e223baa9f161ba
MoMoAxe <tobioluwa348@gmail.com>
2023-11-24 16:06:30 -0800

    puppet: fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Installing flask package with a specific version
+# Installing flask package
 package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
+  ensure 	=> '2.1.0',
+  provider 	=> 'pip3',
 }
