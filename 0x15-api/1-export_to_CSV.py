3f9c1ec0e6eb4e3254e82f846f8b15a95aecc0d4
MoMoAxe <tobioluwa348@gmail.com>
2024-01-02 13:27:01 -0800

    Api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,19 +4,44 @@ This script exports data in csv format
 '''
 
 import csv
-import requests
+import json
 from sys import argv
+from urllib import request
 
 if __name__ == '__main__':
-    uid = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
-    user = requests.get(url, verify=False).json()
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        uid)
-    todo = requests.get(url, verify=False).json()
-    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for t in todo:
-            taskwriter.writerow([int(uid), user.get('username'),
-                                 t.get('completed'),
-                                 t.get('title')])
+    if len(argv) > 1:
+        task_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
+            .format(argv[1])
+        user_url = 'https://jsonplaceholder.typicode.com/users/{}'\
+            .format(argv[1])
+
+        with request.urlopen(task_url) as response:
+            task_data = response.read()
+        with request.urlopen(user_url) as response:
+            user_data = response.read()
+
+        task_data = json.loads(task_data)
+        user_data = json.loads(user_data)
+
+        data = []
+
+        def export_json():
+            '''
+                This function reformats a json data and exports it
+                into a csv file.
+            '''
+
+            for user in task_data:
+                data.append(['\"{}\"'.format(argv[1]),
+                             '\"{}\"'.format(user_data['username']),
+                             '\"{}\"'.format(user['completed']),
+                             '\"{}\"'.format(user['title'])])
+
+            for user in data:
+                print(user)
+
+            with open('{}.csv'.format(argv[1]), 'w', newline='') as csv_file:
+                csv_writer = csv.writer(csv_file)
+                csv_writer.writerows(data)
+
+    export_json()
