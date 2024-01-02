ef1887fc3e5c328668d4b942c1bcdb81321b69dd
MoMoAxe <tobioluwa348@gmail.com>
2024-01-02 11:35:54 -0800

    Api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,32 +8,35 @@ from sys import argv
 from urllib import request
 
 if __name__ == '__main__':
-    task_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos'
-    user_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+    if len(argv) > 1:
+        task_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
+            .format(argv[1])
+        user_url = 'https://jsonplaceholder.typicode.com/users/{}'\
+            .format(argv[1])
 
-    with request.urlopen(task_url) as response:
-        task_data = response.read()
-    with request.urlopen(user_url) as response:
-        user_data = response.read()
+        with request.urlopen(task_url) as response:
+            task_data = response.read()
+        with request.urlopen(user_url) as response:
+            user_data = response.read()
 
-    task_data = json.loads(task_data)
-    user_data = json.loads(user_data)
+        task_data = json.loads(task_data)
+        user_data = json.loads(user_data)
 
-    data = {f"{argv[1]}": []}
+        data = {f"{argv[1]}": []}
 
-    def export_json():
-        '''
-            This function reformats a json data and exports it
-            into a file.
-        '''
+        def export_json():
+            '''
+                This function reformats a json data and exports it
+                into a file.
+            '''
 
-        for user in task_data:
-            data.get(f'{argv[1]}').append(
-                                {"task": user['title'],
-                                    "completed": user['completed'],
-                                    "username": user_data['username']})
+            for user in task_data:
+                data.get(f'{argv[1]}').append(
+                                    {"task": user['title'],
+                                        "completed": user['completed'],
+                                        "username": user_data['username']})
 
-        with open('USER_ID.json', 'w') as json_file:
-            json.dump(data, json_file)
+            with open('{}.json'.format(argv[1]), 'w') as json_file:
+                json.dump(data, json_file)
 
-        export_json()
+    export_json()
