77c092dd8337615efc1f6fcf95a8b1cc440d1ae1
MoMoAxe <tobioluwa348@gmail.com>
2024-01-02 11:28:34 -0800

    Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,32 +8,34 @@ from sys import argv
 from urllib import request
 
 if __name__ == '__main__':
-    task_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos'
-    user_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+    if len(argv) > 1:
+        task_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
+                        .format(argv[1])
+        user_url = 'https://jsonplaceholder.typicode.com/users/{}'\
+            .format(argv[1])
+        with request.urlopen(task_url) as response:
+            task_data = response.read()
+        with request.urlopen(user_url) as response:
+            user_data = response.read()
 
-    with request.urlopen(task_url) as response:
-        task_data = response.read()
-    with request.urlopen(user_url) as response:
-        user_data = response.read()
+        task_data = json.loads(task_data)
+        user_data = json.loads(user_data)
 
-    task_data = json.loads(task_data)
-    user_data = json.loads(user_data)
+        Tasks = []
 
-    Tasks = []
+        def employee_task():
+            '''
+                This fucntion lists an employees data and completed tasks
+            '''
 
-    def employee_task():
-        '''
-            This fucntion lists an employees data and completed tasks
-        '''
+            completed_task = 0
+            for user in task_data:
+                if user['completed']:
+                    completed_task += 1
+                    Tasks.append(user['title'])
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(user_data['name'], completed_task, len(task_data)))
+            for task in Tasks:
+                print(f"\t {task}")
 
-        completed_task = 0
-        for user in task_data:
-            if user['completed']:
-                completed_task += 1
-                Tasks.append(user['title'])
-        print(f"Employee {user_data['name']} is done with\
-                tasks({completed_task}/{len(task_data)}):")
-        for task in Tasks:
-            print(f"\t {task}")
-
-        employee_task()
+    employee_task()
