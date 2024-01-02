029a5a83b504a37fae71bdd8cfb7decf2c6e3690
MoMoAxe <tobioluwa348@gmail.com>
2024-01-02 13:32:15 -0800

    Api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,46 +2,22 @@
 '''
 This script exports data in csv format
 '''
-
 import csv
-import json
-from sys import argv
-from urllib import request
+import requests
+import sys
 
 if __name__ == '__main__':
-    if len(argv) > 1:
-        task_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'\
-            .format(argv[1])
-        user_url = 'https://jsonplaceholder.typicode.com/users/{}'\
-            .format(argv[1])
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    user_name = res.json().get('username')
+    task = url_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
 
-        with request.urlopen(task_url) as response:
-            task_data = response.read()
-        with request.urlopen(user_url) as response:
-            user_data = response.read()
-
-        task_data = json.loads(task_data)
-        user_data = json.loads(user_data)
-
-        data = []
-
-        def export_json():
-            '''
-                This function reformats a json data and exports it
-                into a csv file.
-            '''
-
-            for user in task_data:
-                data.append(['\"{}\"'.format(argv[1]),
-                             '\"{}\"'.format(user_data['username']),
-                             '\"{}\"'.format(user['completed']),
-                             '\"{}\"'.format(user['title'])])
-
-            for user in data:
-                print(user)
-
-            with open('{}.csv'.format(argv[1]), 'w', newline='') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerows(data)
-
-    export_json()
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            title_task = task.get('title')
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
