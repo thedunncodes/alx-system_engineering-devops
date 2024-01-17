ad4e269c483591a0b20af3271f40a50d72132dd7
MoMoAxe <tobioluwa348@gmail.com>
2024-01-17 00:22:29 -0800

    dfgsdfd

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-""" module for function to return number of subreddit subscribers """
+'''
+A script to query reddit api
+'''
+
 import requests
-import sys
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """  Args:
-        subreddit: subreddit name
-    Returns:
-        number of subscribers to the subreddit,
-        or 0 if subreddit requested is invalid"""
-    headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    '''
+    Returns number of subscribers in a subreddit
+    '''
+
+    header = {"user_agent": "Alx_api/1.0"}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=header)
 
     if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
-    return (0)
+        data = response.json()
+        return int(data.get('data').get('subscribers'))
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
