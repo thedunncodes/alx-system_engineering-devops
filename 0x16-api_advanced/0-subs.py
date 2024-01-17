f6b103c259f6cc2cc5c89b2389f878b3ee28327a
MoMoAxe <tobioluwa348@gmail.com>
2024-01-16 23:13:59 -0800

    reddit subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+A script to query reddit api
+'''
+
+from sys import argv
+import requests
+
+
+header = {"user_agent": "Alx_api"}
+subreddit = argv[1]
+
+
+def number_of_subscribers(subreddit):
+    '''
+    Returns number of subscribers in a subreddit
+    '''
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    response = requests.get(url, headers=header)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data.get('data').get('subscribers'))
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(subreddit)
